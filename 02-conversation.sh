afbf27ca17bf771d4d21574b881806f365f17ecf
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-04-24 16:43:44 +0530

    added conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ram:: Hi Suri, hru"
+echo "Suri:: Hello Ram, Iam fine, Hru doing?"
+echo "Ram:: Iam doing good Suri, what is going on"
+echo "Suri:: I started Learning shell Ram "
