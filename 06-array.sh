df162444efd9e8d36101de6285bd4ae6055664d3
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-04-25 16:18:12 +0530

    variables

--- 06-array.sh
+++ 06-array.sh
@@ -4,8 +4,8 @@
 #index starts from 0, size is 3
 FRUITS=("APPLE", "KIWI", "ORANGE")     #Array
 
-echo "First fruit is : ${FRUITs[0]}"
-echo "Second fruit is : ${FRUITs[1]}"
-echo "Third fruit is : ${FRUITs[2]}"
+echo "First fruit is : $FRUITs[0]"
+echo "Second fruit is : $FRUITs[1]"
+echo "Third fruit is : $FRUITs[2]"
 
-echo " fruit is : ${FRUITs[@]}"
+echo " fruit is : $FRUITs[@]"
